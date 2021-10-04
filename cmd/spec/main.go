d2d8bb0cb06e941c449a19f76a45c08f2907bc10
Martin Maroši <marvusm.mmi@gmail.com>
2021-10-04 13:41:25 +0200

    Add openapi gen script.

---
+++ cmd/spec/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/RedHatInsights/quickstarts/pkg/models"
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/getkin/kin-openapi/openapi3gen"
+	"github.com/ghodss/yaml"
+)
+
+type Swagger struct {
+	Components openapi3.Components `json:"components,omitempty" yaml:"components,omitempty"`
+}
+
+// generates openapi schema
+func main() {
+	components := openapi3.NewComponents()
+	components.Schemas = make(map[string]*openapi3.SchemaRef)
+
+	quickstart, _, err := openapi3gen.NewSchemaRefForValue(&models.Quickstart{})
+	checkErr(err)
+	components.Schemas["v1.Quickstart"] = quickstart
+
+	quickstartProgress, _, err := openapi3gen.NewSchemaRefForValue(&models.QuickstartProgress{})
+	checkErr(err)
+	components.Schemas["v1.QuickstartProgress"] = quickstartProgress
+
+	swagger := Swagger{}
+	swagger.Components = components
+
+	b := &bytes.Buffer{}
+	err = json.NewEncoder(b).Encode(swagger)
+	checkErr(err)
+
+	schema, err := yaml.JSONToYAML(b.Bytes())
+	checkErr((err))
+
+	b.Write(schema)
+
+	doc, err := openapi3.NewLoader().LoadFromData(b.Bytes())
+	checkErr(err)
+
+	jsonB, err := doc.MarshalJSON()
+	checkErr(err)
+
+	err = ioutil.WriteFile("./spec/openapi.json", jsonB, 0666)
+	checkErr(err)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/RedHatInsights/quickstarts
 go 1.16
 
 require (
+	github.com/getkin/kin-openapi v0.76.0
+	github.com/ghodss/yaml v1.0.0
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
