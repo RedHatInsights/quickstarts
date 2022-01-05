4ebef1c5173daee6841076f4481380d116e2d1d1
Martin Maroši <marvusm.mmi@gmail.com>
2022-01-05 13:35:57 +0100

    Update build_deploy.sh

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+
 set -exv
 
-IMAGE="quay.io/cloudservices/quickstarts"
+IMAGE="quay.io/cloudservices/insights-ingress"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
-SMOKE_TEST_TAG="latest"
 
 if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
     echo "QUAY_USER and QUAY_TOKEN must be set"
@@ -18,7 +19,7 @@ DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
-docker --config="$DOCKER_CONF" build --no-cache -t "${IMAGE}:${IMAGE_TAG}" .
+docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" .
 docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
-docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:${SMOKE_TEST_TAG}"
-docker --config="$DOCKER_CONF" push "${IMAGE}:${SMOKE_TEST_TAG}"
+docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:qa"
+docker --config="$DOCKER_CONF" push "${IMAGE}:qa"
