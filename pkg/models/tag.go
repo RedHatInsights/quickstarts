90d1b0885e0d9de00d9570ef92c42623f463dccd
Adam Jetmar <ajetmar@redhat.com>
2024-08-15 18:42:05 +0200

    Embed new tags information to quickstarts

--- pkg/models/tag.go
+++ pkg/models/tag.go
@@ -9,11 +9,13 @@ import (
 type TagType string
 
 const (
-	BundleTag      TagType = "bundle"
-	ApplicationTag TagType = "application"
-	ContentKind    TagType = "kind"
-	TopicTag       TagType = "topic"
-	ContentType    TagType = "content"
+	BundleTag       TagType = "bundle"
+	ApplicationTag  TagType = "application"
+	ContentKind     TagType = "kind"
+	TopicTag        TagType = "topic"
+	ContentType     TagType = "content"
+	ProductFamilies TagType = "product-families"
+	UseCase         TagType = "use-case"
 )
 
 func (t *TagType) Scan(value interface{}) error {
@@ -29,7 +31,7 @@ func (t *TagType) Scan(value interface{}) error {
 	tt = TagType(st) //convert type from string to TagType
 
 	switch tt {
-	case BundleTag, ApplicationTag, ContentKind, TopicTag, ContentType: //valid case
+	case BundleTag, ApplicationTag, ContentKind, TopicTag, ContentType, ProductFamilies, UseCase: //valid case
 		*t = tt
 		return nil
 	}
@@ -39,7 +41,7 @@ func (t *TagType) Scan(value interface{}) error {
 func (t TagType) Value() (driver.Value, error) {
 	// only allow enum values
 	switch t {
-	case BundleTag, ApplicationTag, ContentKind, TopicTag, ContentType:
+	case BundleTag, ApplicationTag, ContentKind, TopicTag, ContentType, ProductFamilies, UseCase:
 		return string(t), nil
 	}
 	return nil, errors.New("invalid tag value")
