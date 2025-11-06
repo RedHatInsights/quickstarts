23124bbbe0c96aad4fb09a5ca03e4db611456b49
Cody Mitchell <comitche@redhat.com>
2025-11-06 08:29:22 -0500

    feat: add endpoint for fuzzy search

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -1,10 +1,12 @@
 package database
 
 import (
+	"database/sql"
 	"fmt"
 
 	"github.com/RedHatInsights/quickstarts/config"
 	"github.com/RedHatInsights/quickstarts/pkg/models"
+	"github.com/mattn/go-sqlite3"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
@@ -12,6 +14,16 @@ import (
 )
 
 var DB *gorm.DB
+
+// Register custom Levenshtein function for SQLite
+func init() {
+	sql.Register("sqlite3_custom",
+		&sqlite3.SQLiteDriver{
+			ConnectHook: func(conn *sqlite3.SQLiteConn) error {
+				return conn.RegisterFunc("levenshtein", levenshtein, true)
+			},
+		})
+}
 
 func Init() {
 	var err error
@@ -21,7 +33,11 @@ func Init() {
 
 	var dbdns string
 	if cfg.Test {
-		dia = sqlite.Open(cfg.DbName)
+		// Use custom SQLite driver with Levenshtein function
+		dia = sqlite.Dialector{
+			DriverName: "sqlite3_custom",
+			DSN:        cfg.DbName,
+		}
 	} else {
 		dbdns = fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=%v", cfg.DbHost, cfg.DbUser, cfg.DbPassword, cfg.DbName, cfg.DbPort, cfg.DbSSLMode)
 		if cfg.DbSSLRootCert != "" {
@@ -32,6 +48,11 @@ func Init() {
 	}
 
 	DB, err = gorm.Open(dia, &gorm.Config{})
+
+	// Enable fuzzystrmatch extension for Levenshtein distance fuzzy search
+	if !cfg.Test { // Only enable for non-test environments (PostgreSQL)
+		DB.Exec("CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;")
+	}
 
 	if !DB.Migrator().HasTable(&models.Quickstart{}) {
 		DB.Migrator().CreateTable(&models.Quickstart{})
